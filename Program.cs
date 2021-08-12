36b7eabfa0a5c25d3ad00713a823f63a83514c7a
KoroDomo <100149677@est.uasd.edu.do>
2021-08-12 11:25:51 -0400

    Code added to repo

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Newtech
+{
+    class Animal
+    {
+        public string Raza {get; set;}
+        public int Edad {get; set;}
+
+        public Animal(string pRaza, int pEdad)
+        {
+            pRaza = Raza;
+            pEdad = Edad;
+        }
+        public void PrintAnimal()
+        {
+            Console.WriteLine("Raza = " + Raza + " || " + "Edad = " + Edad);
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Animal a = new Animal("cosa", 13);
+
+            a.PrintAnimal();
+        }
+    }
+}

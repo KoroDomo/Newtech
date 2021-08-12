fcdda51f1ab8406d68a2f3fd336bb234058cffeb
KoroDomo <62364751+KoroDomo@users.noreply.github.com>
2021-08-12 11:39:02 -0400

    Comentarios sobre el problema añadidos
    La funcion PrintAnimal no logra mostrar los valores del objeto creado

--- Program.cs
+++ Program.cs
@@ -6,7 +6,8 @@ namespace Newtech
     {
         public string Raza {get; set;}
         public int Edad {get; set;}
-
+        //el constructor acepta los valores ya que permite la creacion del objeto, pero no retorna los valores de la variables al
+        // momento de llmar la funcion PrintAnimal();
         public Animal(string pRaza, int pEdad)
         {
             pRaza = Raza;
@@ -23,7 +24,7 @@ namespace Newtech
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-
+            //Linea donde se crea el objeto y se le asigna los valores
             Animal a = new Animal("cosa", 13);
 
             a.PrintAnimal();
